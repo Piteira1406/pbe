d183c8e10e39715ffbb8176af1f4d88736001198
Piteira1406 <joaoduarte1406@gmail.com>
2025-06-18 23:59:15 +0100

    modules.py and admin.py modified.

---
+++ pbe/admin.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from .models import ( ClienteProfile, SupplierProfile, ProductCategory, Product, Order, OrderItem )
+
+#-------------------------
+# Cliente
+#-------------------------
+
+@admin.register(ClienteProfile)
+class ClienteProfileAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone')
+    search_fields = ('email__username', 'phone')
+
+#-------------------------
+# Supplier
+#-------------------------
+
+@admin.register(SupplierProfile)
+class SupplierProfileAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone', 'supplier_name')
+    search_fields = ('email__username', 'supplier_name')
+
+#-------------------------
+# Product Category
+#-------------------------
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'description')
+    search_fields = ('category_name',)
+
+#-------------------------
+# Product
+#-------------------------
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock_quantity', 'category', 'supplier', 'sell_unity')
+    search_fields = ('name', 'category__category_name', 'supplier__supplier_name')
+    list_filter = ('category', 'supplier')
+    autocomplete = ['category', 'supplier']
+
+#-------------------------
+# ItemOrder Inline
+#-------------------------
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+#-------------------------
+# Order
+#-------------------------
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cliente', 'status', 'created_at', 'updated_at')
+    search_fields = ('cliente__email__username',)
+    list_filter = ('status', 'created_at')
+    inlines = [OrderItemInline]
+
+    def cliente(self, obj):
+        return obj.cliente.email.get_username() if obj.cliente else 'N/A'
+    cliente.short_description = 'Cliente'
---
+++ pbe/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#--------------------------------
+# CLIENTE PROFILE.
+#--------------------------------
+class ClienteProfile(models.Model):
+    email = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=False, null=False)
+    address = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.email.get_username}'s Profile"
+
+
+#--------------------------------
+# SUPPLIER PROFILE.
+#--------------------------------
+
+class SupplierProfile(models.Model):
+    email = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=False, null=False)
+    supplier_name = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.supplier_name}'s Profile"
+    
+
+#--------------------------------
+# PRODUCT CATEGORIES
+#--------------------------------
+
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.category_name
+    
+#--------------------------------
+# PRODUCTS
+#--------------------------------
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    sell_unity = models.CharField(max_length=50, choices=[('kg', 'Kilogram'),('g', 'Gram'),('l', 'Liter'),('ml', 'Milliliter'),('pcs', 'Pieces')], default='pcs')
+    stock_quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(SupplierProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+#--------------------------------
+# ORDERS
+#--------------------------------
+
+class Order(models.Model):
+    customer = models.ForeignKey(ClienteProfile, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=[('pending', 'Pending'), ('completed', 'Completed'), ('cancelled', 'Cancelled')], default='pending')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer.email.get_username()}"
+    
+
+#--------------------------------
+# ORDER ITEMS
+#--------------------------------
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.id}"
